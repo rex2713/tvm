977c38d5802bf746bdb5cf08a21868c69409e7f1
rex2713 <rex2713@gmail.com>
2024-02-07 10:45:55 +0800

    debug

--- src/page/MemberInfo.jsx
+++ src/page/MemberInfo.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 const MemberInfo = () => {
   window.scrollTo(0, 0);
-  return
-    <main>
-      
-    </main>
+  return <main>MemberInfo</main>;
 };
 
 export default MemberInfo;
