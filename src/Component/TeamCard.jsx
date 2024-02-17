cee3f311b3706661c82c75cb1a96ae6809ed9c75
rex2713 <rex2713@gmail.com>
2024-02-17 16:41:41 +0800

    123

--- src/Component/TeamCard.jsx
+++ src/Component/TeamCard.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 // import required modules
 import { Autoplay } from "swiper/modules";
-import "../css/sweepLight.css";
+import "../css/SweepLight.css";
 
 const TeamCard = () => {
   return (
